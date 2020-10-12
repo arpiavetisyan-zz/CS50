3885bce452dfa3a6069980d9c9fd69c9beaf9af2
Arpi Avetisyan <arpi.avetisyan.1998@gmail.com>
2020-10-12 23:01:30 +0400

    Practicing Python language basics.

---
+++ Lecture2/Condition.py
@@ -0,0 +1,22 @@
+n = int(input("Number: "))
+
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
+
+name = "Harry"
+print(f"First letter of {name} is " + name[0])
+
+names = ["Harry", "Ron", "Hermione"]
+print(names)
+print("Second name in list of names is " + names[1])
+
+# coordinateX = 5
+# coordinateY = 2
+coordinate = (5, 2)
+print(coordinate)
+
+
---
+++ Lecture2/Sets.py
@@ -0,0 +1,15 @@
+s =  set()
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+
+print(s)
+
+s.remove(2)
+
+print(f"The set contains {len(s)} elements.")
+
+print(s)
